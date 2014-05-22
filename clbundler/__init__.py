1838b60a1f43066f952f9178bb7468c32d1e71bc
Peter Lama <peterldev94@gmail.com>
2014-05-22 14:51:56 -0700

    Setup logging

--- clbundler/__init__.py
+++ clbundler/__init__.py
@@ -1,8 +1,19 @@
+import logging
+import sys
 from clbundler.config import global_config
 from clbundler.commandparser import CommandParser
 from clbundler import cli
         
 def main():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(levelname)s: %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    
     parser = CommandParser()
     cli.setup_parser(parser)
     parser.parse_args()
