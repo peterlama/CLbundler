6548f5b5ccc5303a5661b8eef1f49e5b31493c19
Peter Lama <peterldev94@gmail.com>
2014-05-23 13:31:08 -0700

    Add ui for installing a formula kit

--- clbundler/cli.py
+++ clbundler/cli.py
@@ -15,7 +15,7 @@ def on_install(parser, options, args):
     if not args:
         parser.error("no formula specified")
     for n in args:
-        commands.cmd_install(n)
+        commands.cmd_install(n, options.is_kit)
 
 def on_uninstall(parser, options, args):
     if not args:
@@ -39,6 +39,8 @@ def setup_parser(parser):
     subcommand = Subcommand("install", callback=on_install,
                             usage=usage.format("install","FORMULA"),
                             short_help="Install a library into a bundle")
+    subcommand.add_option("--kit", dest="is_kit", action="store_true", default=False,
+                          help="Specify if FORMULA is a kit")
     #subcommand.add_option("-f", "--force", dest="force_install", action="store_true",
     #                      help="Force building formulae")
     parser.add_subcommand(subcommand)
